c61b0eeeff78265e31697792cc3c3ea8ef5084cc
Progetto <vitaliytihonov598@gmail.com>
2026-09-13 22:31:58 +0200

    Install Pillow from Termux packages

--- termux-install.sh
+++ termux-install.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$ROOT_DIR"
 
 pkg update -y
-pkg install -y python git ffmpeg
+pkg install -y python python-pillow git ffmpeg
 python -m pip install --upgrade pip
 python -m pip install -r requirements-termux.txt
 
